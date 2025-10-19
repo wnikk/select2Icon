f494d3d6bcb7f1ab243978569e8e09907d4d2288
Nikolya May <119752633+wnikk@users.noreply.github.com>
2025-10-19 21:15:13 +0100

    Add emoji list (CLDR 47 2025-03-13)

--- icons/index.js
+++ icons/index.js
@@ -1,6 +1,5 @@
 // icons/index.js
-//import emojiList from './emoji.json';
+import emojiList from './emoji-cldr-47.json';
 import fontAwesomeList from './icons-font-awesome-6.6.0-all.json';
 
-//export { emojiList, fontAwesomeList };
-export { fontAwesomeList };
+export { emojiList, fontAwesomeList };
